096a3c4f2ccd3630028d820644a13e35aae0abbc
Twan Kuipers <twan+github@twankuipers.nl>
2026-01-25 10:10:40 +0100

    feat: print command not found

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  char input[256] = {0};
+  fgets(input, sizeof(input), stdin);
+
+  input[strcspn(input, "\n")] = 0;
+  printf("%s: command not found\n", input);
 
   return 0;
 }
