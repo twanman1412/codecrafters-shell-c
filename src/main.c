6696a3164528f49e2cbd69692811cdca80234b54
Twan Kuipers <twan+github@twankuipers.nl>
2026-01-25 15:11:00 +0100

    feat: added support for path executables

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #ifdef _WIN32
 #define os_pathsep ";"
@@ -11,12 +13,16 @@
 #define os_pathsep ":"
 #endif
 
-void echo(const char *str);
-void type(const char *name);
+void echo(char **args);
+void type(char **args);
 
 char* path;
 char** paths;
 char** get_paths();
+
+char* get_command(const char* input);
+char** get_arguments(const char* input);
+char* get_executable(const char* name);
 
 int main(int argc, char *argv[]) {
 	// Flush after every printf
@@ -30,27 +36,54 @@ int main(int argc, char *argv[]) {
 		fgets(input, sizeof(input), stdin);
 
 		input[strcspn(input, "\n")] = 0;
+		const char* cmd = get_command(input);
+		char** args = get_arguments(input);
 
-		if (strcmp(input, "exit") == 0) {
+		if (strcmp(cmd, "exit") == 0) {
 			break;
-		} else if (strcspn(input, "echo ") == 0) {
-			echo(input + 5);
+		} else if (strcmp(cmd, "echo") == 0) {
+			echo(args);
 			continue;
-		} else if (strcspn(input, "type ") == 0) {
-			type(input + 5);
+		} else if (strcmp(cmd, "type") == 0) {
+			type(args);
+			continue;
+		} 
+
+		char* exe = get_executable(cmd);
+		if (exe != NULL) {
+			pid_t pid = fork();
+			if (pid == 0) {
+				// Child process
+				execv(exe, args);
+				exit(0);
+			} else if (pid > 0) {
+				// Parent process
+				int status;
+				waitpid(pid, &status, 0);
+			} else {
+				// Fork failed
+				perror("fork failed");
+				exit(-1);
+			}
+
+			free(exe);
+			free(args);
 			continue;
 		}
+
 		printf("%s: command not found\n", input);
 	}
 
+	free(paths);
 	return 0;
 }
 
-void echo(const char *str) {
-	printf("%s\n", str);
+void echo(char **args) {
+	printf("%s\n", args[0]);
 }
 
-void type(const char *name) {
+void type(char **args) {
+	char* name = args[1]; // first argument is the command name
 	if (strcmp(name, "echo") == 0) {
 		printf("echo is a shell builtin\n");
 	} else if (strcmp(name, "exit") == 0) {
@@ -58,27 +91,72 @@ void type(const char *name) {
 	} else if (strcmp(name, "type") == 0) {
 		printf("type is a shell builtin\n");
 	} else {
-		if (paths == NULL) paths = get_paths();
-		int i = 0;
-		char* path = paths[i];
-		while (path != NULL) {
-			// Allocate space for full path + slash + name + null terminator
-			char* fullpath = malloc(strlen(path) + strlen(name) + 2);
-			sprintf(fullpath, "%s/%s", path, name);
-
-			struct stat perm;
-			if (stat(fullpath, &perm) == 0 && perm.st_mode & S_IXUSR) {
-				printf("%s is %s\n", name, fullpath);
-				free(fullpath);
-				return;
-			}
-
-			free(fullpath);
-			path = paths[++i];
+		char* cmd = get_command(name);
+		char* exe = get_executable(cmd);
+		if (exe != NULL) {
+			printf("%s is %s\n", name, exe);
+			free(exe);
+		} else {
+			printf("%s: not found\n", name);
 		}
 
-		printf("%s: not found\n", name);
+		free(cmd);
 	}
+}
+
+char* get_command(const char* input) {
+	char* input_copy = strdup(input);
+	char* token = strtok(input_copy, " ");
+	char* command = strdup(token);
+	free(input_copy);
+	return command;
+}
+
+char** get_arguments(const char* input) {
+	char* input_copy = strdup(input);
+	 
+	int count = 0;
+	char* token = strtok(input_copy, " "); // Command name is the first argument
+	while (token != NULL) {
+		count++;
+		token = strtok(NULL, " ");
+	}
+
+	// Allocate array of pointers + NULL terminator
+	char** result = malloc((count + 1) * sizeof(char*));
+	input_copy = strdup(input);
+	token = strtok(input_copy, " "); // Skip command
+	int index = 0;
+	while (token != NULL) {
+		result[index++] = strdup(token);
+		token = strtok(NULL, " ");
+	}
+	result[index] = NULL;
+
+	free(input_copy);
+
+	return result;
+}
+
+char* get_executable(const char* name) {
+	if (paths == NULL) paths = get_paths();
+	int i = 0;
+	char* path = paths[i];
+	while (path != NULL) {
+		// Allocate space for full path + slash + name + null terminator
+		char* fullpath = malloc(strlen(path) + strlen(name) + 2);
+		sprintf(fullpath, "%s/%s", path, name);
+
+		struct stat perm;
+		if (stat(fullpath, &perm) == 0 && perm.st_mode & S_IXUSR) {
+			return fullpath;
+		}
+
+		free(fullpath);
+		path = paths[++i];
+	}
+
+	return NULL;
 }
 
 char** get_paths() {
