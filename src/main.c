4ef16df1353c099a17d49f5d6508de78fe9efea1
Twan Kuipers <twankuipers@home.nl>
2026-01-25 10:25:10 +0100

    feat: added type command

--- src/main.c
+++ src/main.c
@@ -2,25 +2,40 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+	// Flush after every printf
+	setbuf(stdout, NULL);
 
-  while (1) {
-	  printf("$ ");
+	while (1) {
+		printf("$ ");
 
-	  char input[256] = {0};
-	  fgets(input, sizeof(input), stdin);
+		char input[256] = {0};
+		fgets(input, sizeof(input), stdin);
 
-	  input[strcspn(input, "\n")] = 0;
+		input[strcspn(input, "\n")] = 0;
 
-	  if (strcmp(input, "exit") == 0) {
-		  break;
-	  } else if (strcspn(input, "echo ") == 0) {
-		  printf("%s\n", input + 5);
-		  continue;
-	  }
-	  printf("%s: command not found\n", input);
-  }
+		if (strcmp(input, "exit") == 0) {
+			break;
+		} else if (strcspn(input, "echo ") == 0) {
+			printf("%s\n", input + 5);
+			continue;
+		} else if (strcspn(input, "type ") == 0) {
+			const char *name = input + 5;
+			if (strcmp(name, "echo") == 0) {
+				printf("echo is a shell builtin\n");
+				continue;
+			} else if (strcmp(name, "exit") == 0) {
+				printf("exit is a shell builtin\n");
+				continue;
+			} else if (strcmp(name, "type") == 0) {
+				printf("type is a shell builtin\n");
+				continue;
+			} else {
+				printf("%s: not found\n", name);
+				continue;
+			}
+		}
+		printf("%s: command not found\n", input);
+	}
 
-  return 0;
+	return 0;
 }
