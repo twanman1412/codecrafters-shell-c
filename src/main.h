7c4a620de2bc33cabdf3e259f91fa9b4c315f250
Twan Kuipers <twan+github@twankuipers.nl>
2026-01-27 21:06:52 +0100

    refactor: added command out and err arguments

--- src/main.h
+++ src/main.h
@@ -5,6 +5,8 @@ struct State {
 	char** paths;
 };
 
+void execute_command(struct State state, char** args, char* out, char* err);
+
 char* get_command(const char* input);
 char** get_arguments(const char* input);
 
